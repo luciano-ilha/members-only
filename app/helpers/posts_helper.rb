6c57586ce2dbbe1ba5b1c559522da0a16beb1d05
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-09 16:05:26 +0100

    Add edit button

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,4 +8,10 @@ module PostsHelper
       link_to 'New Post', new_post_path 
     end
   end
+
+  def edit_btn(post)
+    if user_signed_in?
+      link_to 'Edit',edit_post_path(post) 
+    end
+  end
 end
