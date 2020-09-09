7813f70a9961ffc792871e0911dc0699f1e0b287
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-09 17:27:50 +0100

    remove edit and delete button

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,12 +6,4 @@ module PostsHelper
   def new_post_btn
     return link_to 'New Post', new_post_path if user_signed_in?
   end
-
-  def edit_btn(post)
-    return link_to 'Edit', edit_post_path(post) if user_signed_in?
-  end
-
-  def delete_btn(post)
-    return link_to 'Delete', post, method: :delete, data: { confirm: 'delete this post?' } if user_signed_in?
-  end
 end
