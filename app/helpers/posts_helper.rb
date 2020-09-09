474c66d6566a71f7c36c01b4b4a190518e18edb3
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-09 16:48:25 +0100

    Fix err

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,17 @@
 module PostsHelper
   def user_check(post, _user)
     post.user.name if user_signed_in?
-    end
   end
 
   def new_post_btn
-    return link_to 'New Post', new_post_path unless user_signed_in?
+    return link_to 'New Post', new_post_path if user_signed_in?    
   end
 
   def edit_btn(post)
-    return link_to 'Edit', edit_post_path(post) unless user_signed_in?
+    return link_to 'Edit', edit_post_path(post) if user_signed_in?
   end
 
   def delete_btn(post)
-    return link_to 'Delete', post, method: :delete, data: {confirm: "Are you sure you want to delete this post?"}
-    unless user_signed_in?
+    return link_to 'Delete', post, method: :delete, data: {confirm: 'Are you sure you want to delete this post?'} if user_signed_in?
   end
 end
