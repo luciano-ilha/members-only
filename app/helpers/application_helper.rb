09a2523af9a4d0837cc9711cb3211aabda9b4d45
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-09 13:57:37 +0100

    Change base on  some review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def app_user_check(_post, _user, current_user)
     if user_signed_in?
-      puts current_user
+      current_user
       link_to 'Logout', destroy_user_session_path, method: :delete
     else
       link_to 'Login', new_user_session_path
