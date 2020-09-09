19bc4967f2a3ddc5cf2caf6d3eaf6184f8037b19
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-09 11:17:16 +0100

    Add linters.yml

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :posts, only: %i[new create index destroy]
+  resources :posts, only: %i[new create index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
