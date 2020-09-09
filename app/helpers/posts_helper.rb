bb87a16f7bb01cf6714b4fc26399bb21ef7b3bd8
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-09 15:16:20 +0100

    Add more helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,10 @@ module PostsHelper
   def user_check(post, _user)
     post.user.name if user_signed_in?
   end
+
+  def new_post_btn
+    if user_signed_in?
+      link_to 'New Post', new_post_path 
+    end
+  end
 end
